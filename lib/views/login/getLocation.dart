ca5a0e022874ea9728c9410be15842d8dc674596
mervinkogei <vincent.kogei@yahoo.com>
2023-08-23 15:12:36 +0300

    updated register UI

---
+++ lib/views/login/getLocation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GetLocationScreen extends StatefulWidget {
+  GetLocationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GetLocationScreen> createState() => _GetLocationScreenState();
+}
+
+class _GetLocationScreenState extends State<GetLocationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
