61353b9f944fe0817754a516c0bcfd002da80dd1
mervinkogei <vincent.kogei@yahoo.com>
2023-08-24 20:19:18 +0300

    Enhanced some changes of the dashboard, bottom Nav

---
+++ lib/views/static/scanner.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ScannerScreen extends StatefulWidget {
+  ScannerScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ScannerScreen> createState() => _ScannerScreenState();
+}
+
+class _ScannerScreenState extends State<ScannerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Scanner Screen"),
+      ),
+    );
+  }
+}
