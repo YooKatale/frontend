e66b281d9b2b7a411d177bc8e4f603fb6ba77e51
mervinkogei <vincent.kogei@yahoo.com>
2023-09-06 15:43:56 +0300

    Delivery Address

---
+++ lib/views/Address/deliveryAddress.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DeliveryAddress extends StatefulWidget {
+  DeliveryAddress({Key? key}) : super(key: key);
+
+  @override
+  State<DeliveryAddress> createState() => _DeliveryAddressState();
+}
+
+class _DeliveryAddressState extends State<DeliveryAddress> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: InkWell(
+          onTap: ()=> Navigator.of(context).pop(),
+          child: const Icon(Icons.arrow_back_ios_new_outlined)),
+        title: const Text("Select a Delivery Address",style:TextStyle(color: Colors.black),),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+               Row(
+                 children:const  [
+                  Icon(Icons.circle, color: Colors.black,),
+                  SizedBox(width: 10,),
+                   Text("Recently Used", style: TextStyle(fontSize: 16),),
+                 ],
+               ),
+                      const TextField(
+                        decoration: InputDecoration(
+                          hintText: '86,B/ Ministers Village'
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 0),
+                        child: Center(
+                          child: Container(
+                            width: double.infinity,
+                            height: 45,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5)
+                                ),
+                                backgroundColor: Colors.green.shade600
+                              ),
+                              onPressed: (){}, child: const Text('Delivery to this address', style: TextStyle(color: Colors.white),)),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      Row(
+                 children: [
+                  Icon(Icons.circle, color: Colors.black,),
+                  SizedBox(width: 10,),
+                   Text("Home", style: TextStyle(fontSize: 16),),
+                 ],
+               ),
+                      const TextField(
+                        decoration: InputDecoration(
+                          hintText: '86,B/ Ministers Village'
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Row(
+                 children: const [
+                  Icon(Icons.circle, color: Colors.black,),
+                  SizedBox(width: 10,),
+                   Text("Work", style: TextStyle(fontSize: 16),),
+                 ],
+               ),
+                      const TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Ntinda'
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 0),
+                        child: Center(
+                          child: Container(
+                            width: double.infinity,
+                            height: 50,
+                            child: ElevatedButton.icon(
+                              icon: const Icon(Icons.location_pin, color: Colors.white,),
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5)
+                                ),
+                                backgroundColor: Colors.green.shade600
+                              ),
+                              onPressed: (){}, label: Text('Pick up from store address', style: TextStyle(color: Colors.white),)),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20,),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 0),
+                        child: Center(
+                          child: Container(
+                            width: double.infinity,
+                            height: 50,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5)
+                                ),
+                                backgroundColor: Colors.green.shade600
+                              ),
+                              onPressed: (){}, child: Text('Add a new Address', style: TextStyle(color: Colors.white),)),
+                          ),
+                        ),
+                      ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
