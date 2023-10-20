1b72ca67421c28d5273acaca97c521b70bf5525d
YooKatale <140694504+YooKatale@users.noreply.github.com>
2023-10-20 10:41:04 +0300

    Update Footer.js

--- components/app-icons/Footer.js
+++ components/app-icons/Footer.js
@@ -1,18 +1,1 @@
-import { Box, Text } from "@chakra-ui/react";
-import Link from "next/link";
-import { FaGooglePlay, FaAppStore } from "react-icons/fa";
 
-function FooterIcon() {
-  return (
-    <Box as="footer" textAlign="center" padding="4rem" className="border">
-      <Link href="https://play.google.com/store/apps/details?id=com.yookatale">
-        <FaGooglePlay size={32} color="green" />
-      </Link>
-      <Link href="https://apps.apple.com/app/yookatale/id1234567890">
-        <FaAppStore size={32} color="blue" />
-      </Link>
-    </Box>
-  );
-}
-
-export default FooterIcon;
