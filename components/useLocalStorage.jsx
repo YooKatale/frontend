b866b1e3db9cb0ea31176fcdaf090d4c2ad9eb56
bigmosi <kinyeramo@gmail.com>
2023-10-22 23:59:58 +0300

    remove comments

--- components/useLocalStorage.jsx
+++ components/useLocalStorage.jsx
@@ -4,12 +4,10 @@ import { useState } from "react";
 
 const useLocalStorage = (key, initialValue) => {
   const [state, setState] = useState(() => {
-    // Initialize the state
     try {
       const value =
         typeof window !== "undefined" ? window?.localStorage.getItem(key) : {};
-      // Check if the local storage already has any values,
-      // otherwise initialize it with the passed initialValue
+      
       return value ? JSON.parse(value) : initialValue;
     } catch (error) {
       console.log(error);
