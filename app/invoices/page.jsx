5d3b7c91009f16f2ea0b595ddb42cdfbeb9f77fd
bigmosi <kinyeramo@gmail.com>
2023-10-22 22:50:08 +0300

    commended invoices

--- app/invoices/page.jsx
+++ app/invoices/page.jsx
@@ -1,55 +1,55 @@
-import React from 'react';
+// import React from 'react';
 
 
-const Invoice = () => {
-  return (
-    <div className="p-4 md:p-8">
-      <h1 className="text-2xl md:text-4xl font-bold mb-4">Invoice</h1>
-      <div className="md:flex md:justify-between md:mb-4">
-        <div className="mb-4 md:mb-0">
-          <p className="font-semibold">Invoice Number: 234820s09</p>
-          <p className="font-semibold">Date: 2023 / 5 /11</p>
-        </div>
-        <div>
-          <p className="font-semibold">Bill To:</p>
-          <p>amos</p>
-          <p>krinya</p>
-          <p>
-            kampala
-          </p>
-        </div>
-      </div>
-      <table className="w-full border-collapse border">
-        <thead>
-          <tr className="bg-gray-100">
-            <th className="border px-4 py-2">Description</th>
-            <th className="border px-4 py-2">Quantity</th>
-            <th className="border px-4 py-2">Price</th>
-            <th className="border px-4 py-2">Total</th>
-          </tr>
-        </thead>
-        <tbody>
-            <tr className="border">
-              <td className="border px-4 py-2">Prouct A</td>
-              <td className="border px-4 py-2">Prouct A</td>
-              <td className="border px-4 py-2">$233</td>
-              <td className="border px-4 py-2">$5</td>
-            </tr>
-        </tbody>
-      </table>
-      <div className="mt-4">
-        <p className="text-right">
-          <strong>Subtotal: 200</strong>
-        </p>
-        <p className="text-right">
-          <strong>Tax 10%</strong>
-        </p>
-        <p className="text-right">
-          <strong>Total: $100</strong>
-        </p>
-      </div>
-    </div>
-  );
-};
+// const Invoice = () => {
+//   return (
+//     <div className="p-4 md:p-8">
+//       <h1 className="text-2xl md:text-4xl font-bold mb-4">Invoice</h1>
+//       <div className="md:flex md:justify-between md:mb-4">
+//         <div className="mb-4 md:mb-0">
+//           <p className="font-semibold">Invoice Number: 234820s09</p>
+//           <p className="font-semibold">Date: 2023 / 5 /11</p>
+//         </div>
+//         <div>
+//           <p className="font-semibold">Bill To:</p>
+//           <p>amos</p>
+//           <p>krinya</p>
+//           <p>
+//             kampala
+//           </p>
+//         </div>
+//       </div>
+//       <table className="w-full border-collapse border">
+//         <thead>
+//           <tr className="bg-gray-100">
+//             <th className="border px-4 py-2">Description</th>
+//             <th className="border px-4 py-2">Quantity</th>
+//             <th className="border px-4 py-2">Price</th>
+//             <th className="border px-4 py-2">Total</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//             <tr className="border">
+//               <td className="border px-4 py-2">Prouct A</td>
+//               <td className="border px-4 py-2">Prouct A</td>
+//               <td className="border px-4 py-2">$233</td>
+//               <td className="border px-4 py-2">$5</td>
+//             </tr>
+//         </tbody>
+//       </table>
+//       <div className="mt-4">
+//         <p className="text-right">
+//           <strong>Subtotal: 200</strong>
+//         </p>
+//         <p className="text-right">
+//           <strong>Tax 10%</strong>
+//         </p>
+//         <p className="text-right">
+//           <strong>Total: $100</strong>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Invoice;
+// export default Invoice;
