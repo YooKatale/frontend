0630b051edf18d9f5e78348d25e33f35f84745e4
byasi <solomonbyasi@gmail.com>
2024-02-23 16:57:34 +0300

    added advertisement api

--- app/advertising/page.jsx
+++ app/advertising/page.jsx
@@ -1,26 +1,101 @@
 "use client";
 
-import { Box, Grid, Text, GridItem, Card, CardHeader } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  Text,
+  GridItem,
+  Card,
+  CardHeader,
+  useToast,
+} from "@chakra-ui/react";
 import { SmallCloseIcon, CheckIcon } from "@chakra-ui/icons";
-
-import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
 import MobileView from "@components/advertising/mobile-view";
+import {
+  useAdvertisementPackageGetMutation,
+  useAdvertisementPostMutation,
+} from "@slices/usersApiSlice";
+import { useRouter } from "next/navigation";
 
 const Advertising = () => {
+  const chakraToast = useToast();
+  const router = useRouter();
+  const [advertisementPackages, setAdvertisementPackages] = useState([]);
   const [activeCard, setActiveCard] = useState("basic");
   const [activeButton, setActiveButton] = useState("weekly");
-  const [payment, setPayment] = useState('32,000 ugx');
+  const [payment, setPayment] = useState(0);
+  const [fetchPackages] = useAdvertisementPackageGetMutation();
+  const [createAdvertisement] = useAdvertisementPostMutation();
+  const [isLoading, setIsLoading] = useState(false);
+  const [advertId, setAdvertId] = useState(null);
+  const { userInfo } = useSelector((state) => state.auth);
+
+  // check if user logged in
+  if (!userInfo || userInfo == {} || userInfo == "") {
+    router.push("/signin");
+  }
+
   const handleCardClick = (cardId) => {
     setActiveCard(cardId);
     // setPayment(null)
   };
+
+  const fetchAdvertisementPackages = async (req, res) => {
+    try {
+      const res = await fetchPackages().unwrap();
+      if (res?.success === true) {
+        setAdvertisementPackages(res?.packages);
+      }
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchAdvertisementPackages();
+  }, []);
+
   const handleButtonClick = (buttonId) => {
     setActiveButton(buttonId);
   };
 
+  const handlePayment = async () => {
+    setIsLoading((prev) => (prev ? false : true));
+    try {
+      if (advertId !== null) {
+        const res = await createAdvertisement({
+          user: userInfo._id,
+          packageId: advertId,
+        }).unwrap();
+
+        setIsLoading((prev) => (prev ? false : true));
+        console.log("res", res);
+        if (res.status == "Sucess") router.push(`/payment/${res.data.Order}`);
+      }
+      return;
+    } catch (err) {
+      chakraToast({
+        title: "Error",
+        description: err.data?.message
+          ? err.data?.message
+          : err.data || err.error,
+        status: "error",
+        duration: 5000,
+        isClosable: false,
+      });
+    }
+  };
+
   return (
     <>
-      <MobileView />
+      <MobileView
+        handlePayment={handlePayment}
+        advertisementPackages={advertisementPackages}
+        userInfo={userInfo}
+        setAdvertId={setAdvertId}
+        payment={payment}
+        setPayment={setPayment}
+      />
       <Box className="hidden md:block">
         <h1 className="ml-10 mt-10">Choose the plan that works for you</h1>
         <Box
@@ -405,121 +480,83 @@ const Advertising = () => {
           </Grid>
         </Box>
         {activeCard === "basic" ? (
-          <Box className="w-full sticky flex justify-between h-60 backdrop-blur-md bg-light rounded-md">
-            <div className="flex items-center justify-center ml-10">
-              <button
-                onClick={() => {
-                  handleButtonClick("weekly")
-                  setPayment('32,000 ugx')
-                }}
-                className={`py-2.5 px-10 font-bold me-2 mb-2 text-xl text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 ${
-                  activeButton === "weekly" ? "border-[#ffd900]" : ""
-                }`}
-              >
-                Weekly
-              </button>
-              <button
-                onClick={() => {
-                  handleButtonClick("monthly-basic")
-                  setPayment('92,000 ugx')
-                }}
-                className={`py-2.5 px-10 font-bold me-2 mb-2 text-xl text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 ${
-                  activeButton === "monthly-basic" ? "border-[#ffd900]" : ""
-                }`}
-              >
-                Monthly
-              </button>
-              <button
-                onClick={() => {
-                  handleButtonClick("3months-basic")
-                  setPayment('300,000 ugx')
-                }}
-                className={`py-2.5 px-10 font-bold me-2 mb-2 text-xl text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 ${
-                  activeButton === "3months-basic" ? "border-[#ffd900]" : ""
-                }`}
-              >
-                3 Months
-              </button>
-              <button
-                onClick={() => {
-                  handleButtonClick("6month-basic")
-                  setPayment('530,000 ugx')
-                }}
-                className={`py-2.5 px-10 font-bold me-2 mb-2 text-xl text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 ${
-                  activeButton === "6month-basic" ? "border-[#ffd900]" : ""
-                }`}
-              >
-                6 Months
-              </button>
-              <button
-                onClick={() => {
-                  handleButtonClick("year")
-                  setPayment('1,600,000 ugx')
-                }}
-                className={`py-2.5 px-10 font-bold me-2 mb-2 text-xl text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 ${
-                  activeButton === "year" ? "border-[#ffd900]" : ""
-                }`}
-              >
-                1 Year
-              </button>
-            </div>
-            <div className="flex items-center justify-center">
-              {
-                payment !== null && (
-                  <Text className="py-2.5 px-10 font-bold me-2 mb-2 text-xl text-gray-900 focus:outline-none bg-green border border-gray-200 dark:border-gray-600">
-                {payment}
-              </Text>
-                )
-              }
-            </div>
-          </Box>
+          <>
+            <Box className="w-full sticky flex h-60 backdrop-blur-md bg-light rounded-md">
+              {advertisementPackages
+                .filter((pack) => pack.type === "Basic")
+                .map((pack) => (
+                  <div
+                    className="flex items-center justify-center ml-10"
+                    key={pack._id}
+                  >
+                    <button
+                      onClick={() => {
+                        handleButtonClick(pack.period);
+                        setPayment(pack.price);
+                        setAdvertId(pack._id);
+                      }}
+                      className={`py-2.5 px-10 font-bold me-2 mb-2 text-xl text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 ${
+                        activeButton === pack.period ? "border-[#ffd900]" : ""
+                      }`}
+                    >
+                      {pack.period}
+                    </button>
+                  </div>
+                ))}
+              {advertisementPackages.filter((pack) => pack.type === "Basic")
+                .length > 0 && (
+                <div className="flex items-center ml-[600px] justify-center">
+                  {payment !== null && (
+                    <button
+                      onClick={handlePayment}
+                      className="py-2.5 px-10 font-bold me-2 mb-2 text-xl text-gray-900 focus:outline-none bg-green border border-gray-200 dark:border-gray-600"
+                    >
+                      {payment} ugx
+                    </button>
+                  )}
+                </div>
+              )}
+            </Box>
+          </>
         ) : (
-          <Box className="w-full sticky flex justify-between h-60 backdrop-blur-md bg-light rounded-md">
-            <div className="flex items-center justify-center ml-10">
-              <button
-                onClick={() => {
-                  handleButtonClick("monthly-vip");
-                  setPayment("600,000 ugx");
-                }}
-                className={`py-2.5 px-10 font-bold me-2 mb-2 text-xl text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 ${
-                  activeButton === "monthly-vip" ? "border-[#ffd900]" : ""
-                }`}
-              >
-                Monthly
-              </button>
-              <button
-                onClick={() => {
-                  handleButtonClick("3months-vip");
-                  setPayment("1,600,000 ugx");
-                }}
-                className={`py-2.5 px-10 font-bold me-2 mb-2 text-xl text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 ${
-                  activeButton === "3months-vip" ? "border-[#ffd900]" : ""
-                }`}
-              >
-                3 Months
-              </button>
-              <button
-                onClick={() => {
-                  handleButtonClick("6month-vip");
-                  setPayment("3,200,000 ugx");
-                }}
-                className={`py-2.5 px-10 font-bold me-2 mb-2 text-xl text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 ${
-                  activeButton === "6month-vip" ? "border-[#ffd900]" : ""
-                }`}
-              >
-                6 Months
-              </button>
-            </div>
-            <div className="flex items-center justify-center">
-            {
-                payment !== null && (
-                  <Text className="py-2.5 px-10 font-bold me-2 mb-2 text-xl text-gray-900 focus:outline-none bg-green border border-gray-200 dark:border-gray-600">
-                {payment}
-              </Text>
-                )
-              }
-            </div>
-          </Box>
+          <>
+            <Box className="w-full sticky flex h-60 backdrop-blur-md bg-light rounded-md">
+              {advertisementPackages
+                .filter((pack) => pack.type === "Vip")
+                .map((pack) => (
+                  <div
+                    className="flex items-center justify-center ml-10"
+                    key={pack._id}
+                  >
+                    <button
+                      onClick={() => {
+                        handleButtonClick(pack.period);
+                        setPayment(pack.price);
+                        setAdvertId(pack._id);
+                      }}
+                      className={`py-2.5 px-10 font-bold me-2 mb-2 text-xl text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 ${
+                        activeButton === pack.period ? "border-[#ffd900]" : ""
+                      }`}
+                    >
+                      {pack.period}
+                    </button>
+                  </div>
+                ))}
+              {advertisementPackages.filter((pack) => pack.type === "Vip")
+                .length > 0 && (
+                <div className="flex items-center ml-[600px] justify-center">
+                  {payment !== null && (
+                    <button
+                      onClick={handlePayment}
+                      className="py-2.5 px-10 font-bold me-2 mb-2 text-xl text-gray-900 focus:outline-none bg-green border border-gray-200 dark:border-gray-600"
+                    >
+                      {payment} ugx
+                    </button>
+                  )}
+                </div>
+              )}
+            </Box>
+          </>
         )}
       </Box>
     </>
