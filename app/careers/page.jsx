6313323437f3caccf2078ff2a7a8365d74d37171
MONIL NATHAN WAMALA <119078671+Monilnathan@users.noreply.github.com>
2024-03-13 14:55:01 +0300

    Add files via upload

--- app/careers/page.jsx
+++ app/careers/page.jsx
@@ -4,7 +4,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import Button from "@components/Button";
 
-import { MdOutlineLocationOn } from "react-icons/md";
 
 
 function Careers({ jobs }) {
