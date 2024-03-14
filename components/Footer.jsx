0011f85029fe24ee14ead915b9a9a90f17c20a66
monilnathan <wamalamonil.com>
2024-03-14 07:15:42 +0300

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -321,13 +321,13 @@ const Footer = () => {
                         </Text>
                       </Link>
                     </Box>
-                    <Box margin={"0.3rem 0"}>
-                      <Link href={"/careers"}>
-                        <Text color={ThemeColors.lightColor} _hover={{ color: ThemeColors.darkColor }}>
-                          Careers
-                        </Text>
-                      </Link>
-                    </Box>
+                      <Box margin={"0.3rem 0"}>
+                        <Link href={"/careers"}>
+                          <Text color={ThemeColors.lightColor} _hover={{ color: ThemeColors.darkColor }}>
+                            Careers
+                          </Text>
+                        </Link>
+                      </Box>
                   </Stack>
                 </Box>
                 <Box
