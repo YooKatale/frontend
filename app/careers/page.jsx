9ebdd0dd24968b850e9adc6fd5d5134bc88c40e4
monilnathan <wamalamonil.com>
2024-03-14 07:03:14 +0300

    new careers page

--- app/careers/page.jsx
+++ app/careers/page.jsx
@@ -2,6 +2,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Button from "@components/Button";
+import MdOutlineLocationOn from '@mui/icons-material/OutlineLocationOn';
+
 
 function Careers({ jobs }) {
 
