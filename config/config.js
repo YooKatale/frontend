09f33c9976e514ab4c1e61806816ce3380804e5e
YooKatale <140694504+YooKatale@users.noreply.github.com>
2024-05-27 10:20:39 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const PROD_DB_URL = "https://yookatale-server.onrender.com/api";
 const DEV_DB_URL = "http://localhost:8000/api";
 
-export const DB_URL = PROD_DB_URL;  
+export const DB_URL = DEV_DB_URL;  
 
 
 
