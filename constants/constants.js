f6b0cc1d9fb232f418394223d7cd53734ae58f8c
MomenAbdelwadoud <momenwadoudg@gmail.com>
2024-06-14 15:02:56 +0300

    Added Muslim / pork filter

--- constants/constants.js
+++ constants/constants.js
@@ -12,42 +12,41 @@ import customerServiceIcon from "@public/assets/icons/customer-service.png";
 import cardSecureIcon from "@public/assets/icons/card-secure.png";
 
 export const ThemeColors = {
-  primaryColor: "#185f2d",
-  secondaryColor: "#1f793a",
-  darkColor: "#185f2d",
-  lightColor: "#F0F0F0",
+	primaryColor: "#185f2d",
+	secondaryColor: "#1f793a",
+	darkColor: "#185f2d",
+	lightColor: "#F0F0F0",
 };
 
 export const Images = {
-  logo,
-  logo1,
-  logo2,
-  logo3,
-  img,
-  img1,
-  img2,
-  img3,
-  img4,
-  img5,
-  customerServiceIcon,
-  cardSecureIcon,
+	logo,
+	logo1,
+	logo2,
+	logo3,
+	img,
+	img1,
+	img2,
+	img3,
+	img4,
+	img5,
+	customerServiceIcon,
+	cardSecureIcon,
 };
 
 export const CategoriesJson = [
-  "Carbohydrates",
-  "Protein",
-  "Fats and oils",
-  "Vitamins",
-  "Minerals",
+	"Carbohydrates",
+	"Protein",
+	"Fats and oils",
+	"Vitamins",
+	"Minerals",
+	"Muslim",
 ];
-
 
 export const DisplayImages = [img, img1, img2, img3, img4];
 
 export const Lorem = {
-  short:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eum, voluptatem, corporis qui quo aperiam id mollitia itaque harum quos debitis cupiditate veniam quas incidunt. Harum beatae voluptates hic expedita.",
-  long: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, aliquid aut eveniet cumque inventore accusantium ducimus! Laudantium quam reiciendis velit obcaecati maiores cum necessitatibus error, deleniti eveniet officiis rerum eligendi temporibus. Veniam eaque sunt porro non velit possimus enim numquam, et omnis deleniti beatae adipisci quas dolores officia pariatur magni eius, eum ea soluta perferendis distinctio quasi? Libero mollitia distinctio iste ipsum natus explicabo, illum inventore dolores non perspiciatis error asperiores nulla doloribus repellat est? Illo repudiandae tempore dolore blanditiis optio quo pariatur mollitia libero, dolor dolorem harum quisquam voluptatem? Tenetur, voluptates inventore consequuntur natus eius repudiandae! Debitis ut pariatur ipsam, nemo distinctio dolorem consequuntur mollitia quaerat sunt? Eligendi ea eius dolorem magni iure impedit quam libero voluptatibus praesentium vero accusantium nam, ipsam recusandae cum minus quod cumque aut fugit officia deserunt explicabo quisquam at. Nisi odit a minima molestias ad! Officiis eum nam, ducimus natus voluptatem esse odit cum! Saepe laboriosam aliquid facilis laudantium nihil ipsum similique incidunt quas necessitatibus error asperiores nam atque architecto, numquam quidem, corrupti omnis provident. Cupiditate dolorum ab at eos, dolorem modi labore fugit fugiat. Molestias ratione reprehenderit aliquid maxime ab corrupti obcaecati pariatur, deserunt asperiores quasi et eligendi officiis repudiandae nemo molestiae dolore, voluptatem consequuntur perspiciatis natus. Ex molestiae tempore hic nisi provident, iste quis dolore. Odit voluptatum quod officiis id illo ut suscipit magni delectus, necessitatibus obcaecati expedita, odio tempore dolorum eligendi corporis nemo officia nam nesciunt repudiandae eaque vero tempora quidem! Consectetur, ex. Debitis autem corporis facilis laboriosam porro, totam quod neque libero commodi temporibus nam qui obcaecati animi reiciendis voluptates eveniet quam. Dolore corrupti vero voluptatem neque ipsam reiciendis dolores delectus! Deleniti asperiores excepturi perspiciatis quidem aperiam, vitae autem? Tempore delectus officiis ad blanditiis qui voluptatem consequatur nam exercitationem necessitatibus eos enim, non ducimus molestiae atque, dignissimos totam quas. Inventore quae consequatur facere. Ab aspernatur quae sint, mollitia distinctio quasi consequatur vel qui placeat. Distinctio molestiae cumque eos, libero ipsa unde. A voluptates reiciendis sit voluptatibus dolor necessitatibus quae minima iste distinctio eveniet quia ea commodi officia possimus, consectetur perspiciatis quam error accusamus laboriosam consequuntur at alias mollitia voluptatum! Blanditiis est alias corrupti aut possimus error quia expedita! Dolorum quo rem eum dolor saepe nihil adipisci aspernatur nemo debitis exercitationem ratione corporis dicta a reprehenderit fugit provident delectus laboriosam ad quasi, quis nesciunt doloribus? Pariatur, culpa et quasi minima unde aut ut? Corrupti aperiam atque veniam. Modi, ipsam ut. Corporis.",
+	short: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eum, voluptatem, corporis qui quo aperiam id mollitia itaque harum quos debitis cupiditate veniam quas incidunt. Harum beatae voluptates hic expedita.",
+	long: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, aliquid aut eveniet cumque inventore accusantium ducimus! Laudantium quam reiciendis velit obcaecati maiores cum necessitatibus error, deleniti eveniet officiis rerum eligendi temporibus. Veniam eaque sunt porro non velit possimus enim numquam, et omnis deleniti beatae adipisci quas dolores officia pariatur magni eius, eum ea soluta perferendis distinctio quasi? Libero mollitia distinctio iste ipsum natus explicabo, illum inventore dolores non perspiciatis error asperiores nulla doloribus repellat est? Illo repudiandae tempore dolore blanditiis optio quo pariatur mollitia libero, dolor dolorem harum quisquam voluptatem? Tenetur, voluptates inventore consequuntur natus eius repudiandae! Debitis ut pariatur ipsam, nemo distinctio dolorem consequuntur mollitia quaerat sunt? Eligendi ea eius dolorem magni iure impedit quam libero voluptatibus praesentium vero accusantium nam, ipsam recusandae cum minus quod cumque aut fugit officia deserunt explicabo quisquam at. Nisi odit a minima molestias ad! Officiis eum nam, ducimus natus voluptatem esse odit cum! Saepe laboriosam aliquid facilis laudantium nihil ipsum similique incidunt quas necessitatibus error asperiores nam atque architecto, numquam quidem, corrupti omnis provident. Cupiditate dolorum ab at eos, dolorem modi labore fugit fugiat. Molestias ratione reprehenderit aliquid maxime ab corrupti obcaecati pariatur, deserunt asperiores quasi et eligendi officiis repudiandae nemo molestiae dolore, voluptatem consequuntur perspiciatis natus. Ex molestiae tempore hic nisi provident, iste quis dolore. Odit voluptatum quod officiis id illo ut suscipit magni delectus, necessitatibus obcaecati expedita, odio tempore dolorum eligendi corporis nemo officia nam nesciunt repudiandae eaque vero tempora quidem! Consectetur, ex. Debitis autem corporis facilis laboriosam porro, totam quod neque libero commodi temporibus nam qui obcaecati animi reiciendis voluptates eveniet quam. Dolore corrupti vero voluptatem neque ipsam reiciendis dolores delectus! Deleniti asperiores excepturi perspiciatis quidem aperiam, vitae autem? Tempore delectus officiis ad blanditiis qui voluptatem consequatur nam exercitationem necessitatibus eos enim, non ducimus molestiae atque, dignissimos totam quas. Inventore quae consequatur facere. Ab aspernatur quae sint, mollitia distinctio quasi consequatur vel qui placeat. Distinctio molestiae cumque eos, libero ipsa unde. A voluptates reiciendis sit voluptatibus dolor necessitatibus quae minima iste distinctio eveniet quia ea commodi officia possimus, consectetur perspiciatis quam error accusamus laboriosam consequuntur at alias mollitia voluptatum! Blanditiis est alias corrupti aut possimus error quia expedita! Dolorum quo rem eum dolor saepe nihil adipisci aspernatur nemo debitis exercitationem ratione corporis dicta a reprehenderit fugit provident delectus laboriosam ad quasi, quis nesciunt doloribus? Pariatur, culpa et quasi minima unde aut ut? Corrupti aperiam atque veniam. Modi, ipsam ut. Corporis.",
 };
 
 export const TestBlog = `
